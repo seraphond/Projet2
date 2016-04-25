551d54992d4c3668ba258ad2a7556d8e0f902a62
Seraphond <seraphond@localhost.localdomain>
2016-04-25 09:07:53 +0200

    ajout de result

--- Projet2/result.php
+++ Projet2/result.php
@@ -5,15 +5,27 @@
  * Date: 21/04/16
  * Time: 15:49
  */
+
 include 'function.php';
 
 
 $usr=$_POST['utilisateur'];
 $pwd=$_POST['MDP'];
+$descip=$_POST['description'];
 $mode=$_POST['mode'];
 
 //echo (' '.$usr.' : '.$pwd.' : '.$mode.'');
 
 if($mode ==1){
-   add_utilisateur($usr,$pwd);
+   add_utilisateur($usr,$pwd,$descip);
+}elseif ($mode==0){
+    $verif=check_utilisateur($usr,$pwd);
+    if($verif==1){
+        echo'Utilisateur inconnu où mot de passe incorrect';
+    }
+    if($verif==0){
+        $_SESSION['connected']=true;
+        $_SESSION['login']=$usr;
+        echo 'Bienvenue '.$usr.' .';
+    }
 }
