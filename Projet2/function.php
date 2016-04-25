d0814302856dd005b22db02759e3d38c6b984d08
Seraphond <seraphond@localhost.localdomain>
2016-04-25 08:58:08 +0200

    Commit cette  saloperie de fonction

--- Projet2/function.php
+++ Projet2/function.php
@@ -6,6 +6,7 @@
  * Time: 14:01
  */
 
+session_start();
 
 function form_connect(){
 
@@ -24,6 +25,7 @@ function form_inscription(){
             <form id="inscrip" method="POST" action="result.php">
              Utilisateur: <input type="text" name="utilisateur" required="required"><br>
              Mot de passe: <input type="password" name="MDP" required="required"><br>
+             Description:<TEXTAREA name="description" rows=4 cols=40>Entez votre descrption</TEXTAREA>
             <input type="hidden" name="mode" value="1"/>
              <input type="submit" value="Créer son compte">
             </form>
@@ -37,7 +39,7 @@ function connect(){ //requète Ok.
 
     try{
         $BDD= new PDO('pgsql:host=localhost;port=5432;dbname=sorgniard;user=sorgniard;password=3562Erw$');
-        array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION );
+        $BDD->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
     }catch (Exception $e){
         exit('<b>Catched exception at line '. $e->getLine() .' :</b> '. $e->getMessage());
     }
@@ -45,26 +47,52 @@ function connect(){ //requète Ok.
 
 }
 
-function add_utilisateur($user,$pass){ // La requète fonctionne mais les erreures sont mal gérée ex: le login existe déjà
+function add_utilisateur($user,$pass,$descrip){
     $BDD=connect();
     try{
-        $requete=$BDD->prepare("INSERT INTO utilisateurs (login,pass) VALUES (:login,:pass)");
+        $requete=$BDD->prepare("INSERT INTO utilisateurs (login,pass,description) VALUES (:login,:pass,:descrip)");
         $requete->bindParam(':login',$user,PDO::PARAM_STR);
         $requete->bindParam(':pass',$pass,PDO::PARAM_STR);
+        $requete->bindParam(':descrip',$descrip,PDO::PARAM_STR);
         $requete->execute();
-        array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION );
+
     }catch (Exception $e){
         exit('<b>Catched exception at line '. $e->getLine() .' :</b> '. $e->getMessage());
         
     }
 
 
+    /*INSERT INTO utilisateurs
+VALUES('seraphond','truc,'je met des pantoufles sur ton front ');
+    */
 }
-/*
+
 function check_utilisateur($user,$pass){
     try{
-        $requete=$BDD->prepare('')
-    }catch (Exception $e){
 
+        $BDD=connect();
+        $requete=$BDD->prepare("SELECT login FROM utilisateurs WHERE login=:login and pass=:pass");
+        $requete->bindParam(':login',$user,PDO::PARAM_STR);
+        $requete->bindParam(':pass',$pass,PDO::PARAM_STR);
+        $requete->execute();
+        $data=$requete->fetch();
+        $res=($data==null);
+        return $res;
+
+    }catch (Exception $e){
+        exit('<b>Catched exception at line '. $e->getLine() .' :</b> '. $e->getMessage());
     }
-}*/
+}
+
+/*
+ CREATE TABLE evenement(
+latitude float NOT NULL,
+longitude float NOT NULL,
+titre varchar(40) NOT NULL,
+dateevent date NOT NULL,
+descrptif varchar(1500),
+nb_like int,
+PRIMARY KEY (latitude,longitude,titre,dateevent)
+
+);
+ */
