d9bfef5701f3ee1717976de3b42e8c895560dcc1
seraphond <erwan.sorgniard@gmail.com>
2016-05-11 12:26:23 +0200

    Test du css avec index.php

--- Projet2/index.php
+++ Projet2/index.php
@@ -32,19 +32,40 @@ print_menu();
 ?>
 <div id="carte"></div>
 
-<div id="event">
-    <p>Yolo de thomas</p><p>date, titre, nom de l\'auteur</p>
+<div id="events">
+   <!--<p>Yolo de thomas</p><p>date, titre, nom de l\'auteur</p>-->
     <select>
         <option value="" disabled selected>Ordonner</option>
         <option value="date">Date</option>
         <option value="auteur"><i class="fa fa-users" aria-hidden="true"></i> Auteur</option>
     </select>
+    <div id="event">
+        <div id="titre">Le titre</div>
+        <div id="like">95</div>
+        <div id="date">24/05/2018</div>
+        <div id="longi">24.5</div>
+        <div id="lati">56.8</div>
+        <div id="descr">Je suis une description de ouf !</div>
+    </div>
+    <div id="event">
+        <div id="titre">Le titre</div>
+        <div id="like">95</div>
+        <div id="date">24/05/2018</div>
+        <div id="longi">5546468.5</div>
+        <div id="lati">56.8</div>
+        <div id="descr">Je suis une description de ouf !</div>
+    </div>
 </div>
-
+<?php
+/*
 <pre>Post :<?php print_r($_POST);?></pre>
 <pre>Session:<?php print_r($_SESSION);?></pre>
 <!---<pre><?php print_r($verif);?></pre> --->
 <?php echo($message);?>
+
+*/
+
+?>
 </body>
 
 </html>
